e3a55567047188ab483e981ae9db03820f155293
nATHANnSil <nathanoliveira2002@hotmail.com>
2022-02-11 10:05:02 -0300

    first commit

---
+++ ExemploPOO/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using ExemploPOO.Models;
+
+namespace ExemploPoo
+{
+    class Program
+    {
+        static void Main (string [] args)
+        {
+            pessoa p1= new pessoa();
+            
+            p1.Nome="Nathan";
+            p1.Idade=20;
+
+            p1.Apresentar();
+
+        }
+    }
+}
